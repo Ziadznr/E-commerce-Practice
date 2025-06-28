0aa5e160836df5f6df5a5f56dc1c0a29f6dc38db
Ziad Znr <ziadznr311@gmail.com>
2025-06-28 22:25:02 +0600

    floc update

--- src/controllers/InvoiceController.js
+++ src/controllers/InvoiceController.js
@@ -1,38 +1,49 @@
-const { CreateInvoiceService, PaymentSuccessService, PaymentFailService, PaymentCancelService, PaymentIPNService, InvoiceListService, InvoiceProductListService } = require("../services/InvoiceServices")
+require("dotenv").config(); // Make sure this is called early in your app
 
-exports.CreateInvoice = async(req, res) => {
-    let result = await CreateInvoiceService(req);
-    return res.status(200).json(result)
-}
+const {
+  CreateInvoiceService,
+  PaymentSuccessService,
+  PaymentFailService,
+  PaymentCancelService,
+  PaymentIPNService,
+  InvoiceListService,
+  InvoiceProductListService
+} = require("../services/InvoiceServices");
 
-exports.PaymentSuccess = async(req, res) => {
-    let result = await PaymentSuccessService(req);
-    // Redirect to the frontend (React app) running on port 5173
-    return res.redirect('http://localhost:5173/orders');
+// Default to localhost if not defined
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+exports.CreateInvoice = async (req, res) => {
+  let result = await CreateInvoiceService(req);
+  return res.status(200).json(result);
 };
 
+exports.PaymentSuccess = async (req, res) => {
+  await PaymentSuccessService(req);
+  return res.redirect(`${FRONTEND_URL}/orders`);
+};
 
-exports.PaymentFail = async(req, res) => {
-    let result = await PaymentFailService(req);
-    return res.redirect('http://localhost:5173/orders');
-}
+exports.PaymentFail = async (req, res) => {
+  await PaymentFailService(req);
+  return res.redirect(`${FRONTEND_URL}/orders`);
+};
 
-exports.PaymentCancel = async(req, res) => {
-    let result = await PaymentCancelService(req);
-    return res.redirect('http://localhost:5173/orders');
-}
+exports.PaymentCancel = async (req, res) => {
+  await PaymentCancelService(req);
+  return res.redirect(`${FRONTEND_URL}/orders`);
+};
 
-exports.PaymentIPN = async(req, res) => {
-    let result = await PaymentIPNService(req);
-    return res.status(200).json(result)
-}
+exports.PaymentIPN = async (req, res) => {
+  let result = await PaymentIPNService(req);
+  return res.status(200).json(result);
+};
 
-exports.InvoiceList = async(req, res) => {
-    let result = await InvoiceListService(req);
-    return res.status(200).json(result)
-}
+exports.InvoiceList = async (req, res) => {
+  let result = await InvoiceListService(req);
+  return res.status(200).json(result);
+};
 
-exports.InvoiceProductList = async(req, res) => {
-    let result = await InvoiceProductListService(req);
-    return res.status(200).json(result)
-}
+exports.InvoiceProductList = async (req, res) => {
+  let result = await InvoiceProductListService(req);
+  return res.status(200).json(result);
+};
